dc67aa9e9b1160a6d5757f55dfa42f3c6fa6d9c8
Uriel Salvio <salvio.uriel@gmail.com>
2021-02-07 15:43:37 -0300

    Migrated EventDataObtainer to abstract class to allow removing event from all methods.

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/event/EventDataObtainer.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/event/EventDataObtainer.java
@@ -16,14 +16,19 @@ import org.jetbrains.annotations.Nullable;
  *  All Onyxian plugins that require obtaining data have their unique implementation of this class
  *  Only onyxian plugins should extend this interface directly. All other plugins should extend the implementation located on the api of one of the onyxianPlugins.
  */
-public interface EventDataObtainer {
+public abstract class EventDataObtainer<T extends Event> {
+    private final T e;
+  
+    public EventDataObtainer(T e){
+      this.e = e;
+    }
     
     /**Get the name of the event
      * @param e = an event of this type
      * @return  The name of the event
      */
     @NotNull
-    public default String getEventName(Event e){
+    public String getEventName(){
         return e.getEventName();
     }
     
@@ -32,7 +37,7 @@ public interface EventDataObtainer {
      * @return  The handlers for the event.
      */
     @NotNull
-    public default HandlerList getHandlersList(Event e){
+    public HandlerList getHandlersList(){
         return e.getHandlers();
     }
     
@@ -41,7 +46,7 @@ public interface EventDataObtainer {
      * @return  The class of the event
      */
     @NotNull
-    public default Class<?> getEventClass(Event e){
+    public Class<?> getEventClass(){
         return e.getClass();
     }
     
@@ -50,7 +55,7 @@ public interface EventDataObtainer {
      * @return  true if it has been cancelled, false if it hasn't (or isn't cancellable).
      */
     @NotNull
-    public default boolean isCancelled(Event e){
+    public boolean isCancelled(){
         if (e instanceof Cancellable){
             return ((Cancellable) e).isCancelled();
         }
@@ -68,49 +73,49 @@ public interface EventDataObtainer {
      * @param e = an event of this type
      * @return  The blocks affected by this event.Should always have a block. Can't be null or empty*/
     @Nullable
-    public default List<Block> getAffectedBlocks(Event e) {return null;}
+    public List<Block> getAffectedBlocks() {return null;}
     
     /** Get the Player who triggered the event
      * @param e = an event of this type
      * @return The player who triggered the event. Can be null if the event wasn't caused by a player.
      */
     @Nullable
-    public default Player getTriggerPlayer(Event e) { return null ; }
+    public Player getTriggerPlayer() { return null ; }
        
     /**Get the Explosion cause of this event
      * @param e = an event of this type
      * @return The Entity that generated the explosion. Will be null if it the event wasn't caused by an explosion or if the explosion wasn't caused by an entity.
      */
     @Nullable
-    public default EntityType getExplosionCause(Event e){ return null; }
+    public EntityType getExplosionCause(){ return null; }
     
     /**Get the Exp dropped by the event
      * @param e = an event of this type
      * @return The Entity that generated the explosion. Will be null if it the event doesn't drop xp.
      */
     @Nullable
-    public default Integer getExpDropped(Event e){ return null; }
+    public Integer getExpDropped(){ return null; }
     
     /**Check if the event causes the drop of items.
      * @param e = an event of this type
      * @return true if the event drops items, false if it doesnt
      */
     @NotNull
-    public default boolean isItemDropper(Event e){ return false; }
+    public boolean isItemDropper(){ return false; }
     
     /**Gets the block face which was interacted with (in one way or another), when the event was triggered
      * @param e = an event of this type
      * @return the block face which was interacted with (in one way or another), when the event was triggered.
      */
     @Nullable
-    public default BlockFace getBlockFace(Event e) { return null; } 
+    public BlockFace getBlockFace() { return null; } 
     
     /**Check if the block is instabroken as a result of the event.
      * @param e = an event of this type
      * @return false if the event doesn't break the block instantly or doesn't break the block at all.
      */
     @NotNull
-    public default boolean isBlockInstaBroken(Event e) { return false; } 
+    public boolean isBlockInstaBroken() { return false; } 
     
     /**Get the list of blockStates which are involved on an event. 
      * Blocks represent the current status of a cube in a location of a world.
@@ -121,7 +126,7 @@ public interface EventDataObtainer {
      * @param e = an event of this type
      * @return the list of blockStates which are involved on an event. Usually a single entry, representing the past or future of a cube.
     */
-    public default List<BlockState> getInvolvedStates(Event e){ return null; }
+    public List<BlockState> getInvolvedStates(){ return null; }
 
 
 }
