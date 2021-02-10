d88371fb1d0dbf8a0772673fd30e1c57e66a080a
Uriel Salvio <salvio.uriel@gmail.com>
2021-02-10 18:27:54 -0300

    Added some debug to reflection utils

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/v2/utils/ReflectionUtils.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/v2/utils/ReflectionUtils.java
@@ -8,6 +8,8 @@ public class ReflectionUtils {
 
   public static <T> T create(Class objectClass, Object... constructionArguments) throws IllegalArgumentException{
     try {
+      System.out.println("object class:"+ objectClass + "Construction Arguments:"+ constructionArguments);
+      ConstructorUtils.invokeConstructor(objectClass, constructionArguments);
       return (T) ConstructorUtils.invokeConstructor(objectClass, constructionArguments);
     } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
       throw new IllegalArgumentException("Could not instantiate a "+ objectClass.getSimpleName() + " Utilizing the arguments: "+ Arrays.toString(constructionArguments),e);
