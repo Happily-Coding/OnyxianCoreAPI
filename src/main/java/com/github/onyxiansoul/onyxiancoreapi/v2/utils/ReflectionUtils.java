f2770c7422d727ca11dea03d2133e33dddef3906
Uriel Salvio <salvio.uriel@gmail.com>
2021-02-10 19:16:01 -0300

    Better exceptino handling at create method.

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/v2/utils/ReflectionUtils.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/v2/utils/ReflectionUtils.java
@@ -6,13 +6,20 @@ import org.apache.commons.lang.reflect.ConstructorUtils;
 
 public class ReflectionUtils {
 
+  /**Create an object of a certain class, automatically finding the right constructor.
+   * @param <T> The type of the object.
+   * @param objectClass The class that holds the constructor.
+   * @param constructionArguments The parameters for the construction. CANNOT contain nulls.
+   * @return The created object
+   */
   public static <T> T create(Class objectClass, Object... constructionArguments) throws IllegalArgumentException{
     try {
-      System.out.println("object class:"+ objectClass + "Construction Arguments:"+ constructionArguments);
       ConstructorUtils.invokeConstructor(objectClass, constructionArguments);
       return (T) ConstructorUtils.invokeConstructor(objectClass, constructionArguments);
     } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
       throw new IllegalArgumentException("Could not instantiate a "+ objectClass.getSimpleName() + " Utilizing the arguments: "+ Arrays.toString(constructionArguments),e);
+    } catch(NullPointerException e){
+      throw new IllegalArgumentException("Caught a null pointer while trying to create a " + objectClass.getSimpleName() + " Utilizing the arguments: "+ Arrays.toString(constructionArguments)+ " This is likely the result of passing a null parameter to the create method! That is not allowed!!!");
     } catch (ClassCastException e){
       throw new IllegalArgumentException("Created a  "+ objectClass.getSimpleName() + " Utilizing the arguments: "+ Arrays.toString(constructionArguments) + "But it could not be cast to the required class", e);
     }
