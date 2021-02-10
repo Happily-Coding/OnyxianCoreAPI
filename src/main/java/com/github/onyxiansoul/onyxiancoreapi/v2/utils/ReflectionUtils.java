7e3b18799d12714e8345397f1873e9149ea8401c
Uriel Salvio <salvio.uriel@gmail.com>
2021-02-10 18:19:28 -0300

    Added reflection utils to create from best available constructor.

---
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/v2/utils/ReflectionUtils.java
@@ -0,0 +1,18 @@
+package com.github.onyxiansoul.onyxiancoreapi.v2.utils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import org.apache.commons.lang.reflect.ConstructorUtils;
+
+public class ReflectionUtils {
+
+  public static <T> T create(Class objectClass, Object... constructionArguments) throws IllegalArgumentException{
+    try {
+      return (T) ConstructorUtils.invokeConstructor(objectClass, constructionArguments);
+    } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+      throw new IllegalArgumentException("Could not instantiate a "+ objectClass.getSimpleName() + " Utilizing the arguments: "+ Arrays.toString(constructionArguments),e);
+    } catch (ClassCastException e){
+      throw new IllegalArgumentException("Created a  "+ objectClass.getSimpleName() + " Utilizing the arguments: "+ Arrays.toString(constructionArguments) + "But it could not be cast to the required class", e);
+    }
+  }
+}
