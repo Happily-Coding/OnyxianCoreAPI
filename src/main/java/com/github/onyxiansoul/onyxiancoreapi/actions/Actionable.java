8e13933b1210dc791ead9260138b1201b73e2f3f
Uriel Salvio <salvio.uriel@gmail.com>
2021-02-20 16:11:30 -0300

    Migrated Actions to use runtime suppliers.

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/actions/Actionable.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/actions/Actionable.java
@@ -1,5 +1,6 @@
 package com.github.onyxiansoul.onyxiancoreapi.actions;
 
+import com.github.onyxiansoul.onyxiancoreapi.actions.interpreters.RuntimeSupplier;
 import java.util.Map;
 
 /**Anything that can be executed.
@@ -8,23 +9,18 @@ import java.util.Map;
  *@StaticAction if the action works independently of the run circumstances, for example, it sends a console message*/
  public interface Actionable<T> {
   
-  /**Executes the action, given a set of Circumstances*/
-  //public <T1 extends T> T1 enact(Map<String,Object> runCircumstances) throws ImpossibleActionException;
-  public T enact(Map<String,Object> runCircumstances) throws ImpossibleActionException;
+  /**Executes the action, given a set of Circumstances
+   * @param supplier The runtime supplier, a class that supplies a wide variety of fields, obtaining them from an object used during the supplier creation, such as an event.
+   * @return A variable resulting from the action, or Void if its not meant to be a producer.
+   * @throws com.github.onyxiansoul.onyxiancoreapi.actions.ImpossibleActionException If an action is impossible to perform.
+   */
+  public T enact(RuntimeSupplier supplier) throws ImpossibleActionException;
   
   /**Executes the action, with no circumstances. Might throw an impossibleActionException if it required circumstances*/
   public default T enact()throws ImpossibleActionException{ 
     return enact(null);
   }
-
- /* public default <T1 extends T> T1 enact() throws ImpossibleActionException{
-    return enact(null);
-  }*/
   
-  //TODO: Asegurarse que todos los fields requeridos esten.
-  //public List<String> getRequiredFields
-  
-  //!may not work with enact type change.TODO: test.
   /**Get the type of object this actionable produces
    * @return the class of object this transformer produces*/
   default Class<?> getT() {
