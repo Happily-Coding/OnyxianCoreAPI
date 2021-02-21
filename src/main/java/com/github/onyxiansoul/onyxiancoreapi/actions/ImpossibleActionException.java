a6040e32ee8a8b798ae2b7bafa8a87fc50015435
Uriel Salvio <salvio.uriel@gmail.com>
2021-02-20 22:20:37 -0300

    Changed from exception to throwable in the constructor parameters.

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/actions/ImpossibleActionException.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/actions/ImpossibleActionException.java
@@ -3,8 +3,8 @@ package com.github.onyxiansoul.onyxiancoreapi.actions;
 /**The exception thrown when an action cannot be executed*/
 public class ImpossibleActionException extends Exception {
 
-  public ImpossibleActionException(String reason, Exception e) {
-    super(reason,e);
+  public ImpossibleActionException(String reason, Throwable t) {
+    super(reason,t);
   }
 
   public ImpossibleActionException(String reason) {
