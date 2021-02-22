52b5e06e579f56e9dd6e3ddd50501eba35c9cd3c
Uriel Salvio <salvio.uriel@gmail.com>
2021-02-22 09:17:14 -0300

    Migrations to typed object.

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/configuration/util/ConfigurationUtil.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/configuration/util/ConfigurationUtil.java
@@ -1,5 +1,6 @@
 package com.github.onyxiansoul.onyxiancoreapi.configuration.util;
 
+import com.github.onyxiansoul.onyxiancoreapi.actions.Actionable;
 import com.github.onyxiansoul.onyxiancoreapi.actions.ImpossibleActionException;
 import com.github.onyxiansoul.onyxiancoreapi.configuration.ymlobject.YmlObject;
 import org.bukkit.configuration.ConfigurationSection;
@@ -8,8 +9,8 @@ import org.apache.commons.lang.StringUtils;
 /**Class that contains utility methods to aid in the reading of the configuration*/
 public final class ConfigurationUtil {
   
-    /**Prevent the pointless instatiation of this class*/
-    private ConfigurationUtil(){};
+  /**Prevent the pointless instatiation of this class*/
+  private ConfigurationUtil(){};
 
   public static String getSectionName(ConfigurationSection configSection) {
     String sectionPath = configSection.getCurrentPath();
@@ -19,14 +20,15 @@ public final class ConfigurationUtil {
     return "Configuration Section: {"+sectionPath + "}";
   }
 
-    public static String getActionableType(YmlObject object) throws IllegalArgumentException{
-      try{
-        return (String) object.getField("action type", "action type").enact(null); //Podria ser remplazable por directo invocar string
-      }
-      catch(ClassCastException |ImpossibleActionException | IllegalArgumentException | NullPointerException e){
-        throw new IllegalArgumentException("Could not find the action type of: "+ object.toString(), e);
-      }
+  public static String getActionableType(YmlObject object) throws IllegalArgumentException{
+    try{
+      Actionable<String> field = object.getField("action type", "action type");
+      return field.enact(null);
     }
+    catch(ClassCastException |ImpossibleActionException | IllegalArgumentException | NullPointerException e){
+      throw new IllegalArgumentException("Could not find the action type of: "+ object.toString(), e);
+    }
+  }
 
   public static boolean isLocalReferenceAttempt(String stringToCheck) {
     String homogeniezedStringToCheck = StringUtils.strip(stringToCheck.toUpperCase());
@@ -43,25 +45,5 @@ public final class ConfigurationUtil {
     }
     return false;
   }
-    
-
-    
-    /**Tranforms a section into a LinkedHashMultiMap of its values (String, Value)*/
-    /*public static LinkedHashMultimap<String,LinkedHashMultimap<String,Object>> transformSectionToMap(ConfigurationSection configSection){
-        LinkedHashMultimap<String,LinkedHashMultimap<String,Object>> nameValuesMap = LinkedHashMultimap.create();//Map<ConfigSectionName, configSectionKeyValueMap>
-        LinkedHashMultimap<String,Object> multimapEquivalent = LinkedHashMultimap.create(); //Map<key,value>
-        nameValuesMap.put(configSection.getName(), multimapEquivalent);
-        for(Entry<String,Object> entry: configSection.getValues(false).entrySet()){ //found key x, that is a config section
-            String entryKey = entry.getKey();
-            Object valueToPut = entry.getValue();
-            if(valueToPut instanceof ConfigurationSection){ //Process it if its a configuration section
-                ConfigurationSection valueToPutAsSection =(ConfigurationSection) valueToPut;
-                LinkedHashMultimap<String,LinkedHashMultimap<String,Object>> valueToPutSectionAsMap = transformSectionToMap(valueToPutAsSection);
-                valueToPut = valueToPutSectionAsMap.get(entryKey);
-            }
-            multimapEquivalent.put(entry.getKey(), valueToPut);
-        }
-        return nameValuesMap;
-    }*/
 
 }
