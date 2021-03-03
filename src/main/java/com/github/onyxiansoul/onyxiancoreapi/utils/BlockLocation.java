4c70f29d1bde631bffee2a9da7e3091fc4f96060
Uriel Salvio <salvio.uriel@gmail.com>
2021-03-03 17:28:01 -0300

    Implemented HashCode & Equals so it can be used as key, & toString()

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/utils/BlockLocation.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/utils/BlockLocation.java
@@ -13,5 +13,48 @@ public class BlockLocation implements Serializable {
     this.y = location.getBlockY();
     this.z = location.getBlockZ();
   }
+
+  @Override
+  public int hashCode() {
+    int hash = 5;
+    hash = 13 * hash + this.x;
+    hash = 13 * hash + this.y;
+    hash = 13 * hash + this.z;
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    final BlockLocation other = (BlockLocation) obj;
+    if (this.x != other.x) {
+      return false;
+    }
+    if (this.y != other.y) {
+      return false;
+    }
+    if (this.z != other.z) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("BlockLocation{x=").append(x);
+    sb.append(", y=").append(y);
+    sb.append(", z=").append(z);
+    sb.append('}');
+    return sb.toString();
+  }
   
 }
