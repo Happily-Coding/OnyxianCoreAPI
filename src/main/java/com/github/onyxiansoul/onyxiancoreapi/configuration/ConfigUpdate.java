3c229be8b83cca328204228773c69daa9efb8518
Uriel Salvio <salvio.uriel@gmail.com>
2021-04-18 19:13:40 -0300

    Added a temporairy fix to sometihng that used OnyxianConfiguration

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/configuration/ConfigUpdate.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/configuration/ConfigUpdate.java
@@ -1,5 +1,7 @@
 package com.github.onyxiansoul.onyxiancoreapi.configuration;
 
+import com.github.onyxiansoul.onyxiancoreapi.configuration.ymlobject.YmlObject;
+
 public abstract class ConfigUpdate {
   private final String semanticVersionString;
   
@@ -7,7 +9,7 @@ public abstract class ConfigUpdate {
     this.semanticVersionString = semanticVersionString;
   }
   
-  public abstract void apply(OnyxianConfiguration configurationToApplyTo);
+  public abstract void apply(YmlObject configurationToApplyTo); //TODO: REFACTOR
   
   public final String getVersion(){
     return semanticVersionString;
