a58ab058979edaec6ca7be3b3fae05de50d08d27
Uriel Salvio <salvio.uriel@gmail.com>
2021-04-19 11:34:43 -0300

    Updated YmlObject API naming to be clearer

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/configuration/ymlobject/YmlObject.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/configuration/ymlobject/YmlObject.java
@@ -7,49 +7,78 @@ import org.bukkit.configuration.ConfigurationSection;
 
 public abstract class YmlObject implements Nameable{
 
-  /**Gets the value of a field, or throws an exception if it is invalid or wasn't found
+  /**Gets the value of a field of objectType(that has been registered on the API) or throws an exception if it is invalid or wasn't found
    * @param <T> The type of the field.
    * @param fieldName The name of the field.
-   * @param objectType The name of the type of variable (as it was registered in the API)
+   * @param objectType The name of the type of variable to use to interpret this field (as it was registered in the API)
    * @throws IllegalArgumentException if the field was listed, but is invalid
    * @throws NullPointerException if the field isn't listed.
    * @return the value of the field*/
-  public abstract <T> T getFieldWithType(String fieldName, String objectType) throws IllegalArgumentException, NullPointerException;
+  public abstract <T> T getFieldOfRType(String fieldName, String objectType) throws IllegalArgumentException, NullPointerException;
+  //public abstract <T> T getFieldWithType(String fieldName, String objectType) throws IllegalArgumentException, NullPointerException;
+  
+  /**Gets the value of a field of objectType(that has been registered on the API) or returns a default value if its not available
+   * @param <T> The type of the field.
+   * @param fieldName The name of the field.
+   * @param objectType The name of the type of variable (as it was registered in the API)
+   * @param defaultValue The default value for the field
+   * @return the value of the field or the default value if its not listed
+   * @throws IllegalArgumentException If the field was listed, but is invalid*/
+  public abstract <T> T getFieldOfRTypeOrDefault(String fieldName, String objectType, T defaultValue) throws IllegalArgumentException;
+  //public abstract <T> T getFieldWithTypeOrDefault(String fieldName, String objectType, T defaultValue) throws IllegalArgumentException;
 
+  /**Gets the value of a field of objectType, or returns the value of a registered variable, if the field is not available
+   * @param <T> The type of the field.
+   * @param fieldName The name of the field.
+   * @param objectType The name of the type of variable to use to interpret this field (as it was registered in the API)
+   * @param variableName The name the variable was registered by.
+   * @return the value of the field or the default value if its not listed
+   * @throws IllegalArgumentException If the field was listed, but is invalid*/
+  public abstract <T> T getFieldOfRTypeOrDefaultToVariable(String fieldName, String objectType, String variableName) throws IllegalArgumentException;
+  
+  /** * Returns the value of a yml object's field of objectType.If it doesn't exist, returns the definition of the yml object. for example getFieldOrDefinition("color") in any of this two configs would yield red:
+ MyObject:
+   color: red
+   stripy: true
+ 
+ MyObject: red
+   * 
+   * @param <T> The type of the field.
+   * @param fieldName The name of the field.
+   * @param objectType The name of the type of variable to use to interpret this field
+   * @throws IllegalArgumentException if the field was listed, but is invalid
+   * @throws NullPointerException if the field isn't listed.
+   * @return the value of the field*/
+  public abstract <T> T getFieldOfRTypeOrDefinition(String fieldName, String objectType) throws IllegalArgumentException;
+  
   /**Gets the value of a field, using its fieldName as its objectType. Throws an exception if it the value is invalid or wasn't found
    * @param <T> The type of the field.
    * @param fieldName: The name of the field.
    * @throws IllegalArgumentException if the field was listed, but is invalid
    * @throws NullPointerException if the field isn't listed.
    * @return the value of the field*/
-  public abstract <T> T getField(String fieldName) throws IllegalArgumentException, NullPointerException;
- 
-  /**Gets a field or returns a default value if its not available
-   * @param <T> The type of the field.
-   * @param fieldName The name of the field.
-   * @param objectType The name of the type of variable (as it was registered in the API)
-   * @param defaultValue The default value for the field
-   * @return the value of the field or the default value if its not listed
-   * @throws IllegalArgumentException If the field was listed, but is invalid*/
-  public abstract <T> T getFieldWithTypeOrDefault(String fieldName, String objectType, T defaultValue) throws IllegalArgumentException;
+  public abstract <T> T getRField(String fieldName) throws IllegalArgumentException, NullPointerException;
+  //public abstract <T> T getField(String fieldName) throws IllegalArgumentException, NullPointerException;
 
-  /**Gets a fieldOfInteres, using its fieldName as its objectType, or returns a default value if the field is not available
+  /**Gets the value of a field, using its fieldName as its objectType, or returns a default value if the field is not available
    * @param <T> The type of the field.
    * @param fieldName The name of the field.
    * @param defaultValue The default value for the field
    * @return the value of the field or the default value if its not listed
    * @throws IllegalArgumentException If the field was listed, but is invalid*/
-  public abstract <T> T getFieldOrDefault(String fieldName, T defaultValue) throws IllegalArgumentException, NullPointerException;
+  public abstract <T> T getRFieldOrDefault(String fieldName, T defaultValue) throws IllegalArgumentException, NullPointerException;
+  //public abstract <T> T getFieldOrDefault(String fieldName, T defaultValue) throws IllegalArgumentException, NullPointerException;
 
-  /**Gets a fieldOfInteres, using its fieldName as its objectType, or returns a registered variable, if the field is not available
+  /**Gets the value of a field, using its fieldName as its objectType, or returns the value of a registered variable, if the field is not available
    * @param <T> The type of the field.
    * @param fieldName The name of the field.
-   * @param defaultValue The default value for the field
+   * @param variableName The name the variable was registered by.
    * @return the value of the field or the default value if its not listed
    * @throws IllegalArgumentException If the field was listed, but is invalid*/
-  public abstract <T> T getFieldOrDefaultToVariable(String fieldName, String variableName) throws IllegalArgumentException, NullPointerException;
+  public abstract <T> T getRFieldOrDefaultToVariable(String fieldName, String variableName) throws IllegalArgumentException, NullPointerException;
+  //public abstract <T> T getFieldOrDefaultToVariable(String fieldName, String variableName) throws IllegalArgumentException, NullPointerException;
   
-  /**If the object has a field matching fieldName, returns the value of a field of fieldName, otherwise returns Otherwise, returns the actionable used as definition as if it was the field.
+  /**Returns the value of a yml object's field using its fieldName as its objectType. If it doesn't exist, returns the definition of the yml object.
    * for example getFieldOrDefinition("color") in any of this two configs would yield red:
    * MyObject:
    *   color: red
@@ -62,7 +91,15 @@ public abstract class YmlObject implements Nameable{
    * @throws IllegalArgumentException if the field was listed, but is invalid
    * @throws NullPointerException if the field isn't listed.
    * @return the value of the field*/
-  public abstract <T> T getFieldOrDefinition(String fieldName) throws IllegalArgumentException, NullPointerException;
+  public abstract <T> T getRFieldOrDefinition(String fieldName) throws IllegalArgumentException, NullPointerException;
+  //public abstract <T> T getFieldOrDefinition(String fieldName) throws IllegalArgumentException, NullPointerException;
+  
+  /**Gets a sub YmlObject inside of this one
+   * @param <T> The type of the field
+   * @param fieldKey The key of the field
+   * @return The YmlObject located inside of this one.
+   */
+  public abstract <T> T getYmlObject(String fieldKey) throws IllegalArgumentException, NullPointerException;
   
   /**Gets a list inside this YmlObject
   * @param <T> The type of the elements held in the list.
@@ -79,6 +116,7 @@ public abstract class YmlObject implements Nameable{
   * @return A list of the actionables produced from every value inside the config list, in the order they were on the config list.
   */
   public abstract <T> List<T> getSectionListField(String fieldName, String objectType) throws IllegalArgumentException, NullPointerException;
+  
   
   /**Sets the reference section for this object & completes the YmlObject values using its values for default_from & copy fields
    * This is used by the core but its unlikely that you should need it as an external plugin.
