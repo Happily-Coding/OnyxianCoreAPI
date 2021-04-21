bf7ad0eb7b88b1fc9582a50f233728950b42fd75
Uriel Salvio <salvio.uriel@gmail.com>
2021-04-21 19:08:37 -0300

    Migrated ConfigurationSection parameters to List<YmlObject> parameters to allow multiple references and to be able to pass a reference from our system.

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/configuration/ymlobject/YmlObject.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/configuration/ymlobject/YmlObject.java
@@ -3,80 +3,46 @@ package com.github.onyxiansoul.onyxiancoreapi.configuration.ymlobject;
 import com.github.onyxiansoul.onyxiancoreapi.configuration.exceptions.UnexpectedConfigurationException;
 import java.util.List;
 import com.github.onyxiansoul.onyxiancoreapi.identity.Nameable;
-import org.bukkit.configuration.ConfigurationSection;
 
 public abstract class YmlObject implements Nameable{
 
-  /**Gets the value of a field of objectType(that has been registered on the API) or throws an exception if it is invalid or wasn't found
-   * @param <T> The type of the field.
-   * @param fieldName The name of the field.
-   * @param objectType The name of the type of variable to use to interpret this field (as it was registered in the API)
-   * @param referenceSection
-   * @throws IllegalArgumentException if the field was listed, but is invalid
-   * @throws NullPointerException if the field isn't listed.
-   * @return the value of the field
-   * @throws com.github.onyxiansoul.onyxiancoreapi.configuration.exceptions.UnexpectedConfigurationException*/
-  public abstract <T> T getFieldOfRType(String fieldName, String objectType, ConfigurationSection referenceSection) throws IllegalArgumentException, NullPointerException, UnexpectedConfigurationException;
-  
-  /**Gets the value of a field of objectType(that has been registered on the API) or returns a default value if its not available
-   * @param <T> The type of the field.
-   * @param fieldName The name of the field.
-   * @param objectType The name of the type of variable (as it was registered in the API)
-   * @param referenceSection
-   * @param defaultValue The default value for the field
-   * @return the value of the field or the default value if its not listed
-   * @throws IllegalArgumentException If the field was listed, but is invalid
-   * @throws com.github.onyxiansoul.onyxiancoreapi.configuration.exceptions.UnexpectedConfigurationException*/
-  public abstract <T> T getFieldOfRTypeOrDefault(String fieldName, String objectType, ConfigurationSection referenceSection, T defaultValue) throws IllegalArgumentException, UnexpectedConfigurationException;
-
-  /**Gets the value of a field of objectType, or returns the value of a registered variable, if the field is not available
-   * @param <T> The type of the field.
-   * @param fieldName The name of the field.
-   * @param objectType The name of the type of variable to use to interpret this field (as it was registered in the API)
-   * @param referenceSection
-   * @param variableName The name the variable was registered by.
-   * @return the value of the field or the default value if its not listed
-   * @throws IllegalArgumentException If the field was listed, but is invalid
-   * @throws com.github.onyxiansoul.onyxiancoreapi.configuration.exceptions.UnexpectedConfigurationException*/
-  public abstract <T> T getFieldOfRTypeOrDefaultToVariable(String fieldName, String objectType, ConfigurationSection referenceSection, String variableName) throws IllegalArgumentException, UnexpectedConfigurationException;
-  
   /** * Gets the value of a field, using its fieldName as its objectType.Throws an exception if it the value is invalid or wasn't found
    * @param <T> The type of the field.
    * @param fieldName: The name of the field.
-   * @param referenceSection
+   * @param referencesContainers
    * @throws IllegalArgumentException if the field was listed, but is invalid
    * @throws NullPointerException if the field isn't listed.
    * @return the value of the field
    * @throws com.github.onyxiansoul.onyxiancoreapi.configuration.exceptions.UnexpectedConfigurationException*/
-  public abstract <T> T getRField(String fieldName, ConfigurationSection referenceSection) throws IllegalArgumentException, NullPointerException, UnexpectedConfigurationException;
+  public abstract <T> T getRField(String fieldName, List<YmlObject> referencesContainers) throws IllegalArgumentException, NullPointerException, UnexpectedConfigurationException;
 
   /**Gets the value of a field, using its fieldName as its objectType, or returns a default value if the field is not available
    * @param <T> The type of the field.
    * @param fieldName The name of the field.
    * @param defaultValue The default value for the field
-   * @param referenceSection
+   * @param referencesContainers
    * @return the value of the field or the default value if its not listed
    * @throws IllegalArgumentException If the field was listed, but is invalid
    * @throws com.github.onyxiansoul.onyxiancoreapi.configuration.exceptions.UnexpectedConfigurationException*/
-  public abstract <T> T getRFieldOrDefault(String fieldName, T defaultValue, ConfigurationSection referenceSection) throws IllegalArgumentException, UnexpectedConfigurationException;
+  public abstract <T> T getRFieldOrDefault(String fieldName, T defaultValue, List<YmlObject> referencesContainers) throws IllegalArgumentException, UnexpectedConfigurationException;
 
   /**Gets the value of a field, using its fieldName as its objectType, or returns the value of a registered variable, if the field is not available
    * @param <T> The type of the field.
    * @param fieldName The name of the field.
    * @param variableName The name the variable was registered by.
-   * @param referenceSection
+   * @param referencesContainers
    * @return the value of the field or the default value if its not listed
    * @throws IllegalArgumentException If the field was listed, but is invalid
    * @throws com.github.onyxiansoul.onyxiancoreapi.configuration.exceptions.UnexpectedConfigurationException*/
-  public abstract <T> T getRFieldOrDefaultToVariable(String fieldName, String variableName, ConfigurationSection referenceSection) throws IllegalArgumentException, UnexpectedConfigurationException;
+  public abstract <T> T getRFieldOrDefaultToVariable(String fieldName, String variableName, List<YmlObject> referencesContainers) throws IllegalArgumentException, UnexpectedConfigurationException;
   
   /**Gets a sub YmlObject inside of this one
    * @param <T> The type of the field
    * @param fieldName The key of the field
-   * @param referenceSection
+   * @param referencesContainers
    * @return The YmlObject located inside of this one.
   */
-  public abstract YmlObject getYmlObject(String fieldName, ConfigurationSection referenceSection) throws IllegalArgumentException, NullPointerException, UnexpectedConfigurationException;
+  public abstract YmlObject getYmlObject(String fieldName, List<YmlObject> referencesContainers) throws IllegalArgumentException, NullPointerException, UnexpectedConfigurationException;
   
   /**Gets a list inside this YmlObject
   * @param <T> The type of the elements held in the list.
@@ -86,7 +52,7 @@ public abstract class YmlObject implements Nameable{
   * @return A list of the actionables produced from every value inside the config list, in the order they were on the config list.
   * @throws com.github.onyxiansoul.onyxiancoreapi.configuration.exceptions.UnexpectedConfigurationException
   */
-  public abstract <T> List<T> getListObjects(String fieldName, ConfigurationSection objectTypeReferenceSection, String objectType) throws IllegalArgumentException, NullPointerException, UnexpectedConfigurationException;
+  public abstract <T> List<T> getListObjects(String fieldName, List<YmlObject> referencesContainers, String objectType) throws IllegalArgumentException, NullPointerException, UnexpectedConfigurationException;
   
   /**Gets a list inside this YmlObject
   * @param <T> The type of the elements held in the list.
@@ -94,7 +60,7 @@ public abstract class YmlObject implements Nameable{
   * @param objectType The name of the type of variable inside the list (as it was registered in the API)
   * @return A list of the actionables produced from every value inside the config list, in the order they were on the config list.
   */
-  public abstract <T> List<T> getSectionObjects(String fieldName, ConfigurationSection objectTypeReferenceSection, String objectType) throws IllegalArgumentException, NullPointerException, UnexpectedConfigurationException;
+  public abstract <T> List<T> getSectionObjects(String fieldName, List<YmlObject> referencesContainers, String objectType) throws IllegalArgumentException, NullPointerException, UnexpectedConfigurationException;
 }
 
   /**Returns the value of a yml object's field using its fieldName as its objectType. If it doesn't exist, returns the definition of the yml object.
@@ -127,3 +93,44 @@ public abstract class YmlObject implements Nameable{
    * @throws NullPointerException if the field isn't listed.
    * @return the value of the field*/
   //public abstract <T> T getFieldOfRTypeOrDefinition(String fieldName, String objectType) throws IllegalArgumentException;
+
+
+
+
+
+  //Removed because its unnecesairy since field sinonyms can be registered and might generate confusion.
+  //----------------------------------------------------------------------------------------------------
+
+
+  /**Gets the value of a field of objectType(that has been registered on the API) or throws an exception if it is invalid or wasn't found
+   * @param <T> The type of the field.
+   * @param fieldName The name of the field.
+   * @param objectType The name of the type of variable to use to interpret this field (as it was registered in the API)
+   * @param referencesContainers
+   * @throws IllegalArgumentException if the field was listed, but is invalid
+   * @throws NullPointerException if the field isn't listed.
+   * @return the value of the field
+   * @throws com.github.onyxiansoul.onyxiancoreapi.configuration.exceptions.UnexpectedConfigurationException*/
+  //public abstract <T> T getFieldOfRType(String fieldName, String objectType, List<YmlObject> referencesContainers) throws IllegalArgumentException, NullPointerException, UnexpectedConfigurationException;
+  
+  /**Gets the value of a field of objectType(that has been registered on the API) or returns a default value if its not available
+   * @param <T> The type of the field.
+   * @param fieldName The name of the field.
+   * @param objectType The name of the type of variable (as it was registered in the API)
+   * @param referencesContainers
+   * @param defaultValue The default value for the field
+   * @return the value of the field or the default value if its not listed
+   * @throws IllegalArgumentException If the field was listed, but is invalid
+   * @throws com.github.onyxiansoul.onyxiancoreapi.configuration.exceptions.UnexpectedConfigurationException*/
+  //public abstract <T> T getFieldOfRTypeOrDefault(String fieldName, String objectType, List<YmlObject> referencesContainers, T defaultValue) throws IllegalArgumentException, UnexpectedConfigurationException;
+
+  /**Gets the value of a field of objectType, or returns the value of a registered variable, if the field is not available
+   * @param <T> The type of the field.
+   * @param fieldName The name of the field.
+   * @param objectType The name of the type of variable to use to interpret this field (as it was registered in the API)
+   * @param referencesContainers
+   * @param variableName The name the variable was registered by.
+   * @return the value of the field or the default value if its not listed
+   * @throws IllegalArgumentException If the field was listed, but is invalid
+   * @throws com.github.onyxiansoul.onyxiancoreapi.configuration.exceptions.UnexpectedConfigurationException*/
+  //public abstract <T> T getFieldOfRTypeOrDefaultToVariable(String fieldName, String objectType, List<YmlObject> referencesContainers, String variableName) throws IllegalArgumentException, UnexpectedConfigurationException;
