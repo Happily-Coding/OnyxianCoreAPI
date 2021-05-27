a838f65c59c2b0eabc38882887cbfd035b6d3dec
Uriel Salvio <salvio.uriel@gmail.com>
2021-05-27 14:43:26 -0300

    Fixed missing entry exception message

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/index_system/MissingEntryException.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/index_system/MissingEntryException.java
@@ -5,11 +5,11 @@ import org.jetbrains.annotations.NotNull;
  Its purpose is to force handling of this very real possibility.*/
 public class MissingEntryException extends Exception {
   
-  /**Creates an exception of message: indexName has no entry of name: {entry}
+  /**Creates an exception of message: indexName index has no entry of name: {entry}
    * @param indexName: The name of the index at which the entry is being requested.
    * @param entry: The name of the element being requested.
   */
   public MissingEntryException(@NotNull String indexName, @NotNull String entry){
-    super(indexName + " has no entry of name: {"+entry+"}");
+    super(indexName + " index has no entry of name: {"+entry+"}");
   }
 }
