66747e2e1d9666f5a0ed9197a8e3755b04c628e3
Uriel Salvio <salvio.uriel@gmail.com>
2021-06-08 12:37:34 -0300

    Added EventInterested interface

---
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/actionable_system/EventInterested.java
@@ -0,0 +1,13 @@
+package com.github.onyxiansoul.onyxiancoreapi.actionable_system;
+import com.github.onyxiansoul.onyxiancoreapi.actionable_system.actions.ActionGroup;
+import com.github.onyxiansoul.onyxiancoreapi.identity.Nameable;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Map;
+
+/**Anything that is interested en events*/
+public interface EventInterested extends Actionable<Void>, Serializable, Nameable {
+  
+  public Map<Class, Collection<ActionGroup>> getActionGroupsMap();
+  
+}
