2782d1c23af884ee21fdc18ed211ed1d0acaf60f
Uriel Salvio <salvio.uriel@gmail.com>
2021-06-11 14:53:09 -0300

    Added a method to reactions to check if they are reactive to a certain event

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/actionable_system/Reactions.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/actionable_system/Reactions.java
@@ -3,10 +3,12 @@ import com.github.onyxiansoul.onyxiancoreapi.actionable_system.actions.ActionGro
 import com.github.onyxiansoul.onyxiancoreapi.identity.Nameable;
 import java.io.Serializable;
 
-/**A collection of ActionGroups that want to react to different events*/
+/**A collection of ActionGroups that want to react to different triggers (usually events)*/
 public interface Reactions extends Actionable<Void>, Serializable, Nameable {
   
   /**Adds an ActionGorup to the collection*/
   public void addActionGroup(ActionGroup actionGroup);
   
+  /**Check if the reactions for a reactant(usually event)*/
+  public boolean areReactiveTo(Class trigger);
 }
