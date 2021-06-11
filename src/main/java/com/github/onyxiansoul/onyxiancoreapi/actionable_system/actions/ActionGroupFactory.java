576a8991ca16a9fad230538f1a5c1c1514233036
Uriel Salvio <salvio.uriel@gmail.com>
2021-06-11 15:21:32 -0300

    Changed the class required at action group factory to a class that extends event

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/actionable_system/actions/ActionGroupFactory.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/actionable_system/actions/ActionGroupFactory.java
@@ -1,15 +1,15 @@
 package com.github.onyxiansoul.onyxiancoreapi.actionable_system.actions;
-
 import com.github.onyxiansoul.onyxiancoreapi.actionable_system.Actionable;
 import java.util.List;
+import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public interface ActionGroupFactory {
   /** Gets an ActionGroup containing an action with a single condition */
-  public abstract ActionGroup makeActionGroup(@NotNull Actionable action, @Nullable Actionable<Boolean> condition, @Nullable Class triggerClass, @NotNull String name);
+  public abstract ActionGroup makeActionGroup(@NotNull Actionable action, @Nullable Actionable<Boolean> condition, @Nullable Class<? extends Event> triggerClass, @NotNull String name);
   
   /** Gets an ActionGroup with certain actions and conditions*/
-  public abstract ActionGroup makeActionGroup(@NotNull List<Actionable> actions, @Nullable List<Actionable<Boolean>> conditions, @Nullable Class triggerClass, @NotNull String name);
+  public abstract ActionGroup makeActionGroup(@NotNull List<Actionable> actions, @Nullable List<Actionable<Boolean>> conditions, @Nullable Class<? extends Event> triggerClass, @NotNull String name);
   
 }
