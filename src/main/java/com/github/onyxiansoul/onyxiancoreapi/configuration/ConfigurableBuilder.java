6930245cbf1ac35351121f99f9dd404f3c490686
Uriel Salvio <salvio.uriel@gmail.com>
2021-08-12 14:41:15 -0300

    Changed parameter type to object, since it can automatically wrap values

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/configuration/ConfigurableBuilder.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/configuration/ConfigurableBuilder.java
@@ -1,5 +1,4 @@
 package com.github.onyxiansoul.onyxiancoreapi.configuration;
-import com.github.onyxiansoul.onyxiancoreapi.actionable_system.Actionable;
 import com.github.onyxiansoul.onyxiancoreapi.configuration.exceptions.UnexpectedConfigurationException;
 
 /**Creates a configurable of a certain type*/
@@ -8,7 +7,7 @@ public interface ConfigurableBuilder {
   /**Sets the value of a parameter inside the configurable
    If the value is an actionable, it'll be added as is
    If its not, it'll be wrapped by an actionable, and added.*/
-  public ConfigurableBuilder param(String parameterName, Actionable<?> parameter);
+  public ConfigurableBuilder param(String parameterName, Object parameter);
   
   /**Sets the value of a parameter inside the configurable,
    It interprets the text, as if it was the value of a configuration node*/
