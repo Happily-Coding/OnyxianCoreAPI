fdaf698d8dc897c6d0c8688c767cbbab826b34ab
OnyxianSoul <OnyxianSoul@gmail.com>
2021-09-09 17:06:54 -0300

    Probably fixed the issue with file utils.

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/utils/FileUtils.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/utils/FileUtils.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.util.Objects;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.bukkit.plugin.Plugin;
 
 public final class FileUtils {
@@ -15,28 +13,7 @@ public final class FileUtils {
   public static void unpackOntoPluginFolder(Plugin plugin, String insidePathWithoutFinalBar, String outsidePathWithoutFinalBar, String fileNameWithExtension) throws UnavailableFileException{
     InputStream jarURL;
     try{
-      InputStream jarURL11 = plugin.getResource("/plugin.yml"); 
-      InputStream jarURL22 = plugin.getResource(File.pathSeparator+"plugin.yml");
-      InputStream jarURL23 = plugin.getResource(File.separator+"plugin.yml");
-      InputStream jarURL33 = plugin.getResource("plugin.yml");
-      InputStream jarURL44 = plugin.getClass().getResourceAsStream("plugin.yml");
-      InputStream jarURL55 = plugin.getClass().getResourceAsStream("/plugin.yml");
-      try {
-        InputStream jarURL56 = plugin.getClass().getResource("plugin.yml").openStream();
-      } catch (IOException ex) {
-        Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
-      }
-      
-      InputStream jarURL1 = plugin.getResource(insidePathWithoutFinalBar+"/"+fileNameWithExtension); 
-      InputStream jarURL2 = plugin.getResource("/"+ insidePathWithoutFinalBar+"/"+fileNameWithExtension);
-      InputStream jarURL3 = plugin.getResource("plugin.yml");
-      InputStream jarURL4 = plugin.getClass().getResourceAsStream(fileNameWithExtension);
-      try {
-        InputStream jarURL5 = plugin.getClass().getResource(fileNameWithExtension).openStream();
-      } catch (IOException ex) {
-        Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
-      }
-      jarURL = Objects.requireNonNull(plugin.getClass().getResourceAsStream(insidePathWithoutFinalBar+"/"+fileNameWithExtension));
+      jarURL = Objects.requireNonNull(plugin.getClass().getResourceAsStream("/"+insidePathWithoutFinalBar+"/"+fileNameWithExtension));
     }
     catch(RuntimeException e){
       throw new UnavailableFileException(insidePathWithoutFinalBar, fileNameWithExtension,e);
