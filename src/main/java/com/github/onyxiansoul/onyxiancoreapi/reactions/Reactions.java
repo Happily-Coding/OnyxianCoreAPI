846fa7aead7cf585df0a923e771b0315d8425d2e
OnyxianSoul <OnyxianSoul@gmail.com>
2021-11-16 17:22:04 -0300

    Made reactions not named, since it made no sense that it was.

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/reactions/Reactions.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/reactions/Reactions.java
@@ -1,18 +1,16 @@
 package com.github.onyxiansoul.onyxiancoreapi.reactions;
-import com.github.onyxiansoul.onyxiancoreapi.reactions.ActionGroup;
 import com.github.onyxiansoul.onyxiancoreapi.actionable_system.Actionable;
-import com.github.onyxiansoul.onyxiancoreapi.identity.Nameable;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Map;
 
 /**A collection of ActionGroups that want to react to different triggers (usually events)*/
-public interface Reactions extends Actionable<Void>, Serializable, Nameable {
+public interface Reactions extends Actionable<Void>, Serializable {
   
   /**Adds an ActionGorup to the collection*/
   public void addActionGroup(ActionGroup actionGroup);
   
-  /**Check if the reactions for a reactant(usually event)*/
+  /**Check if the reactions are reactive to something, (usually an event)*/
   public boolean areReactiveTo(Class trigger);
   
   /**Gets the reactions as a map class, action group*/
