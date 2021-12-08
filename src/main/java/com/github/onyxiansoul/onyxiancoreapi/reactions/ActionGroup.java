b89d0dae9b637171272bfe2c209734d3f829af29
OnyxianSoul <OnyxianSoul@gmail.com>
2021-12-08 16:53:58 -0300

    Created StorageHolder interface for any classes that have a storage to store additional variables based on a key, and made actiongroup, and runtime circumstances use it.

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/reactions/ActionGroup.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/reactions/ActionGroup.java
@@ -2,23 +2,18 @@ package com.github.onyxiansoul.onyxiancoreapi.reactions;
 
 import com.github.onyxiansoul.onyxiancoreapi.actionable_system.Actionable;
 import com.github.onyxiansoul.onyxiancoreapi.identity.Nameable;
-import java.util.Map;
-import org.jetbrains.annotations.NotNull;
+import com.github.onyxiansoul.onyxiancoreapi.identity.StorageHolder;
 /** A group of actions that are to be executed under certain conditions.
- * In most cases you will not need to create this,
+ * As a general rule action groups are created each time the plugin configuration is read.
+ *  - This means that this storage will persist across events, however
+ *  - This also means that this storage will not persist across server restarts, or configuration reloads
+ * In most cases you will not need to create an action group,
  * But in particular cases, such as expanding an action map to allow friendlier configuration, you might.
  * To make one, you can use @link ActionGroupFactory.class
  */
-public interface ActionGroup extends Actionable<Void>, Nameable{
+public interface ActionGroup extends Actionable<Void>, Nameable, StorageHolder<String, Object>{
   
   /**Gets the class that this action group is meant to react to*/
   Class<?> getTriggerClass();
   
-  /**Gets the storage of the action group.
-   * Action groups are created each time the plugin configuration is read.
-   * This means that this storage will persist across events, however
-   * This also means that this storage will not persist across server restarts, or configuration reloads
-   * Also please keep in mind this method may be moved to @link Actionable in the future.
-   */
-  public @NotNull Map<String,?> getStorage();
 }
