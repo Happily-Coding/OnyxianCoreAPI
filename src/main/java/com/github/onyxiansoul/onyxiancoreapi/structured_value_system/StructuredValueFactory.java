4bb1373aedf1e6d773fd9ed5df2bd796ae0cd0e8
OnyxianSoul <OnyxianSoul@gmail.com>
2022-09-10 19:39:54 -0300

    Removed unnecesairy methods from structured value factory.

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/structured_value_system/StructuredValueFactory.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/structured_value_system/StructuredValueFactory.java
@@ -8,11 +8,15 @@ import org.jetbrains.annotations.Nullable;
 /**The place that allows creating structured values*/
 public interface StructuredValueFactory {
   
+  /**Creates a structured value with no parent, a certain key, and a value depending on the value object
+   * @param key The key of the structured value.
+   * @param value Any type of value.
+    * If its a List, Map, or Memory Section, it will be transformed into a list of fields the new structured value will hold.
+    * If its a structured value, it will be used as a single field inside the new structured value.
+    * If its any other kind of value, it will be used as a non structured final value of the created structured value. 
+   * @return  the new structured value
+   */
   public abstract StructuredValue create(@NotNull String key, @Nullable Object value);
-  
-  public abstract StructuredValue create (@NotNull Object value);
-  
-  public abstract StructuredValue create();
   
   /**Load a configuration from a file in the plugin folder, creating that file from the template inside the jar if it doesn't exist.
   * previously called getConfiguration, and previously located at OnyxianCoreAPI
