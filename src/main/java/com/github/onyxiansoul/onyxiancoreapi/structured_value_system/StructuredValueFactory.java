44ac1ee791ebaead1837f902120ca6707d41b735
OnyxianSoul <OnyxianSoul@gmail.com>
2022-09-12 15:26:56 -0300

    Modified the structured value factory to make sure the process of creating it is more modular.

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/structured_value_system/StructuredValueFactory.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/structured_value_system/StructuredValueFactory.java
@@ -8,15 +8,16 @@ import org.jetbrains.annotations.Nullable;
 /**The place that allows creating structured values*/
 public interface StructuredValueFactory {
   
-  /**Creates a structured value with no parent, a certain key, and a value depending on the value object
+  /**Creates a structured value with a certain parent, a certain key, and a value depending on the value object
    * @param key The key of the structured value.
    * @param value Any type of value.
+   * @param parent The parent of this structured value (can be null if it shouldn't have one)
     * If its a List, Map, or Memory Section, it will be transformed into a list of fields the new structured value will hold.
     * If its a structured value, it will be used as a single field inside the new structured value.
     * If its any other kind of value, it will be used as a non structured final value of the created structured value. 
    * @return  the new structured value
    */
-  public abstract StructuredValue create(@NotNull String key, @Nullable Object value);
+  public abstract StructuredValue create(@Nullable StructuredValue parent, @NotNull String key, @Nullable Object value);
   
   /**Load a configuration from a file in the plugin folder, creating that file from the template inside the jar if it doesn't exist.
   * previously called getConfiguration, and previously located at OnyxianCoreAPI
