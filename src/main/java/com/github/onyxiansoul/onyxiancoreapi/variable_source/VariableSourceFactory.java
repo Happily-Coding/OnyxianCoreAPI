29a14a1daab00261fce0252ee47f7cc1cc4d3d80
OnyxianSoul <OnyxianSoul@gmail.com>
2022-09-13 11:09:00 -0300

    Fixed an error in the javadocs

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/variable_source/VariableSourceFactory.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/variable_source/VariableSourceFactory.java
@@ -12,12 +12,12 @@ public interface VariableSourceFactory {
   public abstract VariableSource makeVariableSource(StructuredValue structuredValue);
   
   /**A utility method that calls many factory methods commonly used in a chain to get a variable source from a configuration file (file> yaml configuration > structured value > variable source)
-   * @param fileNameWithExtension the name of the file to grab, with its extension for example config.yml
+   * @param fileNameWithoutExtension the name of the file to grab, without its extension for example config (instead of config.yml)
    * @param pluginFolderSubPath the path where the file should be located in the plugin folder, for example "" for the file to be in the main folder, or "configs" to be inside the configs folder, or "configs/world" to be inside the world folder inside configs.
    * @param pluginJarSubPath the path of the file inside the jar, for example "" for the file to be in the main folder, or "configs" to be inside the configs folder, or "configs/world" to be inside the world folder inside configs.
    * @return The file, located outside the plugin folder.
    * @throws com.github.onyxiansoul.onyxiancoreapi.files.UnavailableFileException If the file couldn't be obtained.
    * @throws com.github.onyxiansoul.onyxiancoreapi.configuration.exceptions.UnexpectedConfigurationException if it couldn't be interpreted.
    */
-  public VariableSource makeVariableSourceFromFile(@NotNull String fileNameWithExtension, @NotNull String pluginFolderSubPath, @NotNull String pluginJarSubPath) throws UnavailableFileException, UnexpectedConfigurationException;
+  public VariableSource makeVariableSourceFromFile(@NotNull String fileNameWithoutExtension, @NotNull String pluginFolderSubPath, @NotNull String pluginJarSubPath) throws UnavailableFileException, UnexpectedConfigurationException;
 }
