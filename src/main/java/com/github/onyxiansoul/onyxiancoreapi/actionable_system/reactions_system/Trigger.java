dd5e40c2d3426a901d6b922a671c0ee974e62f67
OnyxianSoul <OnyxianSoul@gmail.com>
2022-09-28 16:50:28 -0300

    Added impossible action exception to the trigger, since getting the class can fail

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/actionable_system/reactions_system/Trigger.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/actionable_system/reactions_system/Trigger.java
@@ -2,10 +2,13 @@
 package com.github.onyxiansoul.onyxiancoreapi.actionable_system.reactions_system;
 
 import com.github.onyxiansoul.onyxiancoreapi.actionable_system.Actionable;
+import com.github.onyxiansoul.onyxiancoreapi.actionable_system.ImpossibleActionException;
 import org.bukkit.event.Event;
 
 public interface Trigger extends Actionable<Boolean> {
   
-  public Class<? extends Event> getEventClass();
+  /**Gets the event class configured inside this trigger
+   @throws ImpossibleActionException if the actionable thats supossed to give the event class fails to execute.*/
+  public Class<? extends Event> getEventClass() throws ImpossibleActionException;
 
 }
