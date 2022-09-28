97b958af55421b3644aee5dc2a5c7acd50a4d74b
OnyxianSoul <OnyxianSoul@gmail.com>
2022-09-28 17:21:59 -0300

    Added impossible action exception to the trigger, since getting the class can fail

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/actionable_system/reactions_system/Reaction.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/actionable_system/reactions_system/Reaction.java
@@ -1,11 +1,12 @@
 package com.github.onyxiansoul.onyxiancoreapi.actionable_system.reactions_system;
 
+import com.github.onyxiansoul.onyxiancoreapi.actionable_system.ImpossibleActionException;
 import com.github.onyxiansoul.onyxiancoreapi.actionable_system.runtime_circumstances_system.ActionCollection;
 
 /** An element that can react to an object of some type, usually an event.
  */
 public interface Reaction extends ActionCollection{
   
-  public Class<?> getTriggerClass();
+  public Class<?> getTriggerClass() throws ImpossibleActionException;
   
 }
