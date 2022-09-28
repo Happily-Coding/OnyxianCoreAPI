740c4a8e94962af700e5fb40bbd1249353486764
OnyxianSoul <OnyxianSoul@gmail.com>
2022-09-28 12:21:00 -0300

    Made ActionCollection return a boolean instead of void, since sometimes its important to know if the collection actually triggered (for example in Reaction)

--- src/main/java/com/github/onyxiansoul/onyxiancoreapi/actionable_system/runtime_circumstances_system/ActionCollection.java
+++ src/main/java/com/github/onyxiansoul/onyxiancoreapi/actionable_system/runtime_circumstances_system/ActionCollection.java
@@ -5,7 +5,8 @@ import com.github.onyxiansoul.onyxiancoreapi.actionable_system.Actionable;
 import com.github.onyxiansoul.onyxiancoreapi.identity.Nameable;
 import com.github.onyxiansoul.onyxiancoreapi.identity.StorageHolder;
 
-/**A collection of actions that can be executed at once, and can hold values shared across actions, in order to allow them to cooperate*/
-public interface ActionCollection extends Nameable, Actionable<Void>, StorageHolder<String,Object>{
+/**A collection of actions that can be executed at once, and can hold values shared across actions, in order to allow them to cooperate
+ Should return true, if the enaction resulted in the action's activation*/
+public interface ActionCollection extends Nameable, Actionable<Boolean>, StorageHolder<String,Object>{
 
 }
